9983d17a9dfba291de46c3b2cd37356f14dba753
rsmith-finix <rsmith@finixcreative.com>
2016-02-16 13:04:59 -0800

    Login - prompt and checkbox
    Use prompts to toggle checkbox state to true

--- message-check.js
+++ message-check.js
@@ -1,16 +1,27 @@
 var user = false;
+
+var message = prompt("Enter your message:", "\"I'm gonna make him an offer he can't refuse.\"");
+var username = prompt("You must be signed in to use this feature. If you have an account, enter your username:", "Don Corleone");
+var password = prompt("Thank you. Now enter your password:", "godfather123");
+
 function userSignIn(){
 	user = !user;
 };
+
 function messageCheck(){
 	var chat = function(){
 		// return chat popup
-		prompt("Enter your message:", "\"I'm gonna make him an offer he can't refuse.\"");
+		message;
 	};
 	var login = function(){
 		// return login popup
-		prompt("You must be signed in to use this feature. If you have an account, enter your username:", "Don Corleone");
-		prompt("Thank you. Now enter your password:", "godfather123");
+		username;
+		password;
+		if(username !== null && password !== null){
+			user = true;
+		} else {
+			user = false;
+		}
 	};
 	if(user !== false){ //user authentication check
 		chat();
