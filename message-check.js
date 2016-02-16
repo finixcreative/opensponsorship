69b07b851f269f8e0ea2ac8dd67a3df237664dd1
rsmith-finix <rsmith@finixcreative.com>
2016-02-16 13:44:02 -0800

    Message Check - username authentication
    Set user authentication to check input for empty strings

--- message-check.js
+++ message-check.js
@@ -10,12 +10,13 @@ function messageCheck(){
 	function chat(){
 		// return chat popup
 		user.message = prompt("Enter your message:", "\"I'm gonna make him an offer he can't refuse.\"");
+
 	};
 	function login(){
 		// return login popup
 		user.username = prompt("You must be signed in to use this feature. If you have an account, enter your username:", "Don Corleone");
 		user.password = prompt("Thank you. Now enter your password:", "godfather123");
-		if(user.username !== null && user.password !== null){
+		if(user.username !== "" && user.password !== ""){
 			user.isSignedIn = true;
 			document.getElementById("login").checked = true;
 		} else {
