c1b7a706129340f21524cd9a531a3e8e2e92d193
rsmith-finix <rsmith@finixcreative.com>
2016-02-16 13:18:44 -0800

    Checkbox - data binding
    Apply data binding to checkbox element

--- message-check.js
+++ message-check.js
@@ -1,29 +1,30 @@
-var user = false;
-
-var message = prompt("Enter your message:", "\"I'm gonna make him an offer he can't refuse.\"");
-var username = prompt("You must be signed in to use this feature. If you have an account, enter your username:", "Don Corleone");
-var password = prompt("Thank you. Now enter your password:", "godfather123");
+var user = {
+	isSignedIn: ko.observable(false),
+	message: prompt("Enter your message:", "\"I'm gonna make him an offer he can't refuse.\""),
+	username: prompt("You must be signed in to use this feature. If you have an account, enter your username:", "Don Corleone"),
+	password: prompt("Thank you. Now enter your password:", "godfather123")
+};
 
 function userSignIn(){
-	user = !user;
+	user.isSignedIn = !user.isSignedIn;
 };
 
 function messageCheck(){
 	var chat = function(){
 		// return chat popup
-		message;
+		user.message;
 	};
 	var login = function(){
 		// return login popup
-		username;
-		password;
-		if(username !== null && password !== null){
-			user = true;
+		user.username;
+		user.password;
+		if(user.username !== null && user.password !== null){
+			user.isSignedIn = true;
 		} else {
-			user = false;
+			user.isSignedIn = false;
 		}
 	};
-	if(user !== false){ //user authentication check
+	if(user.isSignedIn !== false){ //user authentication check
 		chat();
 	} else {
 		login();
